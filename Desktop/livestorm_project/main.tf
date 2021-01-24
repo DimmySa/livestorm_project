8cd0c1b9e4b752b82a8e62becb127965a98400a6
DimmySa <dimitrios.sarri@yahoo.com>
2021-01-24 22:50:36 +0100

    new file:   ansible.cfg 	new file:   files/index.html 	new file:   main.tf 	new file:   nginx.yml 	new file:   provider.tf 	new file:   variable.tf

---
+++ Desktop/livestorm_project/main.tf
@@ -0,0 +1,65 @@
+# Create an EC2 instane
+locals {
+  vpc_id = "vpc-7e8da117"
+  subnet_id = "subnet-18094371"
+  ssh_user = "dimmy"
+  key_name = "my_new_key"
+  private_key_path = "~/livestorm_project/terraform/my_new_key.pem" 
+}
+
+
+resource "aws_instance" "nginx" {
+  instance_type = "${var.instance_type}"
+  ami = "${var.ami}"
+  subnet_id = "subnet-18094371" 
+  associate_public_ip_address = true 
+  security_groups = [aws_security_group.nginx.id]
+  key_name = local.key_name
+  tags = {
+      Name = "${var.tag}"
+  }
+  provisioner "remote-exec" {
+    inline = ["echo 'wait for ssh to become ready'"]
+    
+    connection {
+      type = "ssh"
+      user = "local.ssh_user"
+      private_key = file(local.private_key_path)
+      host =  aws_instance.nginx.public_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook -i ${aws_instance.nginx.public_ip},  --private-key ${local.private_key_path} nginx.yml"
+  }
+}  
+
+output "nginx_ip" {
+  value = aws_instance.nginx.public_ip
+}
+
+
+resource "aws_security_group" "nginx" {
+  name = "nginx_access"
+  vpc_id = local.vpc_id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks =  ["0.0.0.0/0"]
+  }
+}
---
+++ Desktop/livestorm_project/variable.tf
@@ -0,0 +1,16 @@
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami" {
+  default = "ami-0ba7c4110ca9bfe0b"
+}
+
+variable "tag" {
+    default = "terra_ec2_web_app"
+}
+
+variable "region" {
+  default = "eu-west-3"
+}
+
